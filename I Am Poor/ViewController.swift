81ccf37774e3b04bbd0855881c469b8f4c7be8fa
Louis Nguyen <ndmn004@gmail.com>
2024-01-17 11:39:02 +0700

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Louis on 17/1/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
